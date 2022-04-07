7f54187c8f8f09b9dfee69577c4f3c2e72cb463b
Daniil Ognevoy <roxxor503@gmail.com>
2022-04-07 12:35:49 +0300

    styled code with eslint and prettier

--- src/app/components/pagination.jsx
+++ src/app/components/pagination.jsx
@@ -1,21 +1,41 @@
 import React from "react";
-import _ from 'lodash'
+import _ from "lodash";
+import PropTypes from "prop-types";
 
-const Pagination = ({itemsCount, pageSize, onPageChange, currentPage}) => {
-    const pageCount = Math.ceil(itemsCount / pageSize)
-    if (pageCount === 1) return null
-    const pages = _.range(1, pageCount + 1)
+const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
+    const pageCount = Math.ceil(itemsCount / pageSize);
+    if (pageCount === 1) return null;
+    const pages = _.range(1, pageCount + 1);
+
     return (
         <nav>
             <ul className="pagination">
                 {pages.map((page) => (
-                    <li className={'page-item ' + (page === currentPage ? 'active' : '')} key={"page_" + page}>
-                        <button className="page-link" onClick={() => {
-                            onPageChange(page)
-                        }}>{page}</button>
-                    </li>))}
+                    <li
+                        className={
+                            "page-item " +
+                            (page === currentPage ? "active" : "")
+                        }
+                        key={"page_" + page}
+                    >
+                        <button
+                            className="page-link"
+                            onClick={() => {
+                                onPageChange(page);
+                            }}
+                        >
+                            {page}
+                        </button>
+                    </li>
+                ))}
             </ul>
         </nav>
-    )
-}
-export default Pagination
+    );
+};
+Pagination.propTypes = {
+    itemsCount: PropTypes.number,
+    pageSize: PropTypes.number,
+    onPageChange: PropTypes.func.isRequired,
+    currentPage: PropTypes.number
+};
+export default Pagination;
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,40 +1,57 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import User from "./user";
 import Pagination from "./pagination";
 import { paginate } from "../utils/paginate";
+import PropTypes from "prop-types";
 
-const Users = ({users, onDelete, onToggleBookMark}) => {
-    const count = users.length
-    const pageSize = 4
-    const [currentPage, setCurrentPage] = useState(1)
+const Users = ({ users, onDelete, onToggleBookMark }) => {
+    const count = users.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
     const handlePageChange = (pageIndex) => {
-        setCurrentPage(pageIndex)
-    }
-
-    const userCrop = paginate(users, currentPage, pageSize)
+        setCurrentPage(pageIndex);
+    };
+    const userCrop = paginate(users, currentPage, pageSize);
 
     return (
         <>
-            {count > 0 && (<table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">Имя</th>
-                    <th scope="col">Качества</th>
-                    <th scope="col">Профессия</th>
-                    <th scope="col">Встретился, раз</th>
-                    <th scope="col">Оценка</th>
-                    <th scope="col">Избранное</th>
-                    <th scope="col"/>
-                </tr>
-                </thead>
-                <tbody>
-                {userCrop.map((user) => <User key={user._id} user={user} onDelete={onDelete}
-                                           onToggleBookMark={onToggleBookMark}/>)}
-                </tbody>
-            </table>)}
-            <Pagination itemsCount={count} pageSize={pageSize} currentPage={currentPage}
-                        onPageChange={handlePageChange}/>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th scope="col" />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userCrop.map((user) => (
+                            <User
+                                key={user._id}
+                                user={user}
+                                onDelete={onDelete}
+                                onToggleBookMark={onToggleBookMark}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
         </>
-    )
-}
-export default Users
+    );
+};
+Users.propTypes = {
+    users: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired
+};
+export default Users;
