26b915fdda6e5a3611dcb976972b798c78efb5e5
Daniil Ognevoy <roxxor503@gmail.com>
2022-04-12 15:47:22 +0300

    error checking

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Users from "./components/users";
 import api from "./api";
 
 const App = () => {
     const [users, setUsers] = useState(api.users.fetchAll());
+    console.log(users);
+    useEffect(() => {
+        api.users.fetchAll().then((data) => {
+            setUsers(data);
+        });
+    });
 
     const handleDelete = (userId) => {
         setUsers((prevState) => {
--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -20,7 +20,7 @@ GroupList.defaultProps = {
     contentProperty: "name"
 };
 GroupList.propTypes = {
-    items: PropTypes.object.isRequired,
+    items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
     valueProperty: PropTypes.string.isRequired,
     contentProperty: PropTypes.string.isRequired,
     onItemSelect: PropTypes.func,
