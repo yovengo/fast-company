79c6725856ff0116d405d1c825b85b27c23ba741
Daniil Ognevoy <roxxor503@gmail.com>
2022-04-12 14:47:14 +0300

    added filter (groupList.jsx) component

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Users from "./components/users";
-import SearchStatus from "./components/searchStatus";
 import api from "./api";
 
 const App = () => {
@@ -25,7 +24,6 @@ const App = () => {
 
     return (
         <>
-            <SearchStatus length={users.length} />
             <Users
                 users={users}
                 onDelete={handleDelete}
