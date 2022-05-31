43a511c23a42b9c67a6094ae64a53fc420500c03
Daniil Ognevoy <roxxor503@gmail.com>
2022-05-31 17:13:54 +0300

    added quality.service.js & useQualities.jsx

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { ProfessionProvider } from "./hooks/useProfession";
+import { QualityProvider } from "./hooks/useQualities";
 
 import Users from "./layouts/users";
 import Login from "./layouts/login";
@@ -12,14 +13,19 @@ function App() {
     return (
         <div>
             <NavBar />
-            <ProfessionProvider>
-                <Switch>
-                    <Route path="/users/:userId?/:edit?" component={Users} />
-                    <Route path="/login/:type?" component={Login} />
-                    <Route path="/" exact component={Main} />
-                    <Redirect to="/" />
-                </Switch>
-            </ProfessionProvider>
+            <QualityProvider>
+                <ProfessionProvider>
+                    <Switch>
+                        <Route
+                            path="/users/:userId?/:edit?"
+                            component={Users}
+                        />
+                        <Route path="/login/:type?" component={Login} />
+                        <Route path="/" exact component={Main} />
+                        <Redirect to="/" />
+                    </Switch>
+                </ProfessionProvider>
+            </QualityProvider>
             <ToastContainer />
         </div>
     );
---
+++ src/app/hooks/useQualities.jsx
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import qualityService from "../services/quality.service";
+import { toast } from "react-toastify";
+
+const QualityContext = React.createContext();
+
+export const useQualities = () => {
+    return useContext(QualityContext);
+};
+
+export const QualityProvider = ({ children }) => {
+    const [qualities, setQualities] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        getQualitiesList();
+    }, []);
+
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+
+    function getQuality(id) {
+        return qualities.find((q) => q._id === id);
+    }
+
+    async function getQualitiesList() {
+        try {
+            const { content } = await qualityService.get();
+            setQualities(content);
+            setLoading(false);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+
+    return (
+        <QualityContext.Provider value={{ isLoading, qualities, getQuality }}>
+            {children}
+        </QualityContext.Provider>
+    );
+};
+QualityProvider.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
